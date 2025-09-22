8e8c0920a562eff3b1f7cfe0238e3fbe7349b64e
Harsh Yadav <rao.harsh597@gmail.com>
2025-09-22 15:31:26 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, enableIndexedDbPersistence, FirestoreError } from "firebase/firestore";
 
 const firebaseConfig = {
-  projectId: "adfun-rewards-dl24p",
-  appId: "1:334398680055:web:768bd863f8d54780205cf2",
-  storageBucket: "adfun-rewards-dl24p.firebasestorage.app",
-  apiKey: "AIzaSyDAOjQziyoVzeqHFfOQIXld-7LF4-nLAp4",
-  authDomain: "adfun-rewards-dl24p.firebaseapp.com",
-  messagingSenderId: "334398680055",
+  "projectId": "adfun-rewards-dl24p",
+  "appId": "1:334398680055:web:768bd863f8d54780205cf2",
+  "apiKey": "AIzaSyDAOjQziyoVzeqHFfOQIXld-7LF4-nLAp4",
+  "authDomain": "adfun-rewards-dl24p.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "334398680055",
+  "storageBucket": "adfun-rewards-dl24p.appspot.com"
 };
 
 // Initialize Firebase
