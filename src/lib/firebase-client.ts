c85d25022c73c993d42cb96a08aeea728d4a80f5
Harsh Yadav <rao.harsh597@gmail.com>
2025-09-22 15:50:28 +0000

    [2025-09-22T15:43:20.574Z]  @firebase/firestore: Firestore (10.14.1): We

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "adfun-rewards-dl24p",
-  "appId": "1:334398680055:web:768bd863f8d54780205cf2",
-  "apiKey": "AIzaSyDAOjQziyoVzeqHFfOQIXld-7LF4-nLAp4",
-  "authDomain": "adfun-rewards-dl24p.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "334398680055",
-  "storageBucket": "adfun-rewards-dl24p.appspot.com"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
